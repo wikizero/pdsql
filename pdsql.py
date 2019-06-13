f41de8b20ed681c5a54b484244baaad9fb6c2933
attitudes <937316808@qq.com>
2019-06-13 22:26:20 +0800

    update function

--- pdsql.py
+++ pdsql.py
@@ -11,9 +11,9 @@ class PdSQLAccessor():
     def __init__(self, pandas_obj):
         self.df = pandas_obj
 
-    def mapping_df_types(self, df):
+    def mapping_df_types(self):
         dtypedict = {}
-        for i, j in zip(df.columns, df.dtypes):
+        for i, j in zip(self.df.columns, self.df.dtypes):
             if "object" in str(j):
                 dtypedict.update({i: NVARCHAR(length=255)})
             if "float" in str(j):
@@ -22,18 +22,29 @@ class PdSQLAccessor():
                 dtypedict.update({i: Integer()})
         return dtypedict
 
-    def to_sql(self, name, con, index=False, chunksize=10 ** 5, primary_key=None, **kwargs):
+    def to_sql(self, table, con, index=False, chunksize=10 ** 5, primary_key=None, **kwargs):
         if self.df.empty:
             print('DataFrame is empty!')
             return
 
-        dtype = kwargs.get('dtype', self.mapping_df_types(self.df))
-        self.df.to_sql(name, con, index=index, chunksize=chunksize, dtype=dtype)
+        if primary_key and primary_key not in self.df.columns:
+            raise Exception(f"Key column '{primary_key}' doesn't exist in table")
+
+        # default data type
+        dtype = self.mapping_df_types()
+        if 'dtype' in kwargs:
+            dtype = dtype.update(kwargs.pop('dtype'))
+
+        # call pandas to_sql function
+        self.df.to_sql(table, con, index=index, chunksize=chunksize, dtype=dtype, **kwargs)
 
         # set primary key
-        if primary_key:
-            with con.connect() as con:
-                con.execute(f'ALTER TABLE `{name}` ADD PRIMARY KEY (`{primary_key}`);')
+        with con.connect() as con:
+            query_cur_primary_sql = f"SELECT column_name FROM INFORMATION_SCHEMA.`KEY_COLUMN_USAGE` WHERE table_name='{table}' AND constraint_name='PRIMARY'"
+            ret = con.execute(query_cur_primary_sql)
+            cur_primary_keys = list(ret)[0].values()
+            if primary_key and primary_key not in cur_primary_keys:
+                con.execute(f'ALTER TABLE `{table}` ADD PRIMARY KEY (`{primary_key}`);')
 
     def insert(self, table, con, conflict='fail', limit=10 ** 5):
         if self.df.empty:
@@ -66,8 +77,31 @@ class PdSQLAccessor():
 
         return True
 
-    def update(self):
-        pass
+    def update(self, table, con, condition, limit=10 ** 5):
+
+        source = self.df.to_dict(orient='records')
+
+        condition = [condition] if isinstance(condition, str) else condition
+
+        # sort the columns
+        columns = source[0].keys()
+        set_columns = list(set(columns) - set(condition))
+        columns = set_columns + condition
+
+        set_str = ','.join([c + '=%s' for c in set_columns])
+        condition_str = ' AND '.join([c + '=%s' for c in condition])
+
+        values = [[dct[column] for column in columns] for dct in source]
+
+        sql = 'UPDATE ' + table + ' SET ' + set_str + ' WHERE ' + condition_str
+
+        for index in range(0, len(values), limit):
+            insert_values = values[index:index + limit]
+            with con.connect() as con:
+                ret = con.execute(sql, insert_values)
+            print(f'Insert {len(insert_values)} into {table}, {ret.rowcount} rows effected')
+
+        return True
 
 
 if __name__ == '__main__':
@@ -77,23 +111,26 @@ if __name__ == '__main__':
     now = datetime.now().__str__()
     source = [
         {
-            'id': 10,
-            'task': 'taa1',
+            'id': 1211,
+            'task': 'xxtaa1',
             'reason': now,
         },
         {
-            'id': 11,
-            'task': 'task2',
+            'id': 1111,
+            'task': 'xxtask2',
             'reason': now,
         },
         {
-            'id': 14,
-            'task': 'tasa3',
+            'id': 1114,
+            'task': 'xxtasa3',
             'reason': now,
         },
     ]
     df = DataFrame(source)
     engine = create_engine("mysql://root:root@localhost:3306/test?charset=utf8")
-    # df.pdsql.to_sql('myTable', engine, primary_key='id')
-    df.pdsql.insert('myTable', engine, conflict='ignore')
-    # TODO remove duplicate entry warning
+    # df.pdsql.to_sql('myTable', engine, primary_key='id', if_exists='append')
+    # df.pdsql.insert('myTable', engine, conflict='ignore')
+    df.pdsql.update('myTable', engine, condition=['id', 'task'])
+
+    # TODO Using Transactions
+
